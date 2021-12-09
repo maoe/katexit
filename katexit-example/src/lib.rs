aea384268d22f33dbbe94888de030b6356367839
Mitsutoshi Aoe <me@maoe.name>
2021-12-09 11:57:47 +0900

    Add a faling module-level doc

--- katexit-example/src/lib.rs
+++ katexit-example/src/lib.rs
@@ -1,4 +1,7 @@
 #[cfg_attr(doc, katexit::katexit)]
+//! Can we write $\LaTeX$ expressions at the module-level doc?
+
+#[cfg_attr(doc, katexit::katexit)]
 /// We can write $\LaTeX$ expressions
 ///
 /// Display style
